ffc48c986eb2c501cc2a5bc634e257880c8fad97
Annie Lydens <aelydens@gmail.com>
2018-11-20 21:13:00 -0600

    Add basic About page

--- src/main.js
+++ src/main.js
@@ -1,11 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import cactus from "./cactus.png";
+
 class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>hello!</h1>
+        <div className="container flexCenter fullHeight">
+          <div className="main">
+            <div className="navigation">
+              <ul>
+                <li className="active">About</li>
+                <li>Work</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+
+            <div className="greeting">
+              <p className="title">
+                Hi, I'm Annie.
+                <img src={cactus} className="cactus" alt="cactus" />
+              </p>
+              <p>
+                I write code at
+                <a href="https://thoughtbot.com/" className="link">
+                  thoughtbot
+                </a>{" "}
+                in Austin, TX.
+              </p>
+              <p>
+                I previously worked at
+                <a href="https://www.techstars.com/" className="link">
+                  Techstars
+                </a>
+                ,
+                <a href="https://www.kiva.org/" className="link">
+                  Kiva
+                </a>
+                , and TechSoup.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
